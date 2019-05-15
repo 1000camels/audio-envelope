3ed2cbe086ceb19e63b9e143ead22c50b539aafa
Darcy Christ <darcy@aporia.info>
2019-05-15 14:34:30 +1000

    Updating version number

--- audio-envelope.php
+++ audio-envelope.php
@@ -1,14 +1,7 @@
 <?php
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
  * @link    https://aporia.info
- * @since   1.0.0
+ * @since   1.0.1
  * @package Audio_Envelope
  *
  * @wordpress-plugin
@@ -34,7 +27,7 @@ if ( ! defined('WPINC') ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('AUDIO_ENVELOPE_VERSION', '1.0.0');
+define('AUDIO_ENVELOPE_VERSION', '1.0.1');
 
 /**
  * The code that runs during plugin activation.
