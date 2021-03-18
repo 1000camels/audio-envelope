3e04ade415e8eee9f10d30650fb81a6443c432f0
Darcy Christ <darcy@aporia.info>
2021-03-19 10:18:48 +1100

    updating npm libraries;making sure elementor widget doesn't throw errors

--- elementor/init.php
+++ elementor/init.php
@@ -121,19 +121,19 @@ final class Elementor_Audio_Envelope {
 
       // Check if Elementor installed and activated
       if ( ! did_action( 'elementor/loaded' ) ) {
-         add_action( 'admin_notices', [ $this, 'admin_notice_missing_main_plugin' ] );
+         //add_action( 'admin_notices', [ $this, 'admin_notice_missing_main_plugin' ] );
          return;
       }
 
       // Check for required Elementor version
       if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
-         add_action( 'admin_notices', [ $this, 'admin_notice_minimum_elementor_version' ] );
+         //add_action( 'admin_notices', [ $this, 'admin_notice_minimum_elementor_version' ] );
          return;
       }
 
       // Check for required PHP version
       if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
-         add_action( 'admin_notices', [ $this, 'admin_notice_minimum_php_version' ] );
+         //add_action( 'admin_notices', [ $this, 'admin_notice_minimum_php_version' ] );
          return;
       }
 
